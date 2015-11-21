ebfbeb74620a533da326b38202ad4495feea843c
DeadRisingCell <alexandrudinu99@yahoo.com>
2015-11-21 23:15:04 +0200

    Managed to resize map, draw squares, taking care of boundaries

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 package platform.javagithub;
 
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 
 import platform.javagithub.SoftPie;
 
 public class Main {
+	
+	public static int dim = 20;
 	//color
 	class Color
 	{
@@ -17,13 +20,12 @@ public class Main {
 			this.a = a;
 		}
 	}
-	
 	Color c = new Color(0, 150, 0, 1);
 	Color c2 = new Color(120, 0, 0, 1);
-	
-	int mousePos[][] = new int[32][32];
-	int width;
-	int height;
+
+	int width = soft.getWidth()/dim;
+	int height = soft.getHeight()/dim;
+	int mousePos[][] = new int[width+1][height+1];
 	
 	public static SoftPie soft = new SoftPie();
 	//drawing a pixel
@@ -53,56 +55,65 @@ public class Main {
 	}
 	public int[][] rm(int[][] v)
 	{
-		int temp[][] = new int[v.length][v[0].length];
 		for(int i=0;i<=v.length-1;i++)
 			for(int j=0;j<=v[0].length-1;j++)
-				temp[i][j] = -1;
-		return temp;
+				v[i][j] = -1;
+		return v;
 	}
-	/*public void dbsos(MouseComponents[] v, int mposx, int mposy, int w)
+	public void addtts(int v[][], int mposx, int mposy)
 	{
-		if(v.length == 0)
-			return;
 		if(Mouse.isButtonDown(0))
-		{
-			for(int i=1;i<=v.length;i++)
-				{
-				if(((v[i].pos1 == -1) && (v[i].pos2 == -1))||(v[i].pos1 != mposx) && (v[i].pos2) != mposy)
-					{   v[i].pos1 = mposx;
-						v[i].pos2 = mposy;}
-				}
-		}
-		for(int i=1;i<=v.length;i++)
-			if((v[i].pos1 != -1) && (v[i].pos2 != -1))
-				dfsq(w*mposx, w*mposy, w,1, c2);
-	}*/
+			if(mposx <= width && mposy <= height)
+			 if(v[mposx][mposy] == -1)
+				 v[mposx][mposy] = 1;
+	}
+	public void dms(int v[][], int w)
+	{
+		for(int i=0;i<=v.length-1;i++)
+			for(int j=0;j<=v[0].length-1;j++)
+				if(v[i][j] == 1)
+					dfsq(i * w, j * w, w+1, 1, c2);
+	}
 	public void initFuncs()
 	{
 		soft.init();
     	mousePos = rm(mousePos);
 	}
-	public void runnable(int w)
+	public void runnable(int dim)
 	{
-		width = soft.getWidth()/w;
-		height = soft.getHeight()/w;
 //---------------------------------
 		for(int x=0;x<=width;x++)
 			for(int y=0;y<=height;y++)
-     			desq(x * w, y *w ,w , 1);
+     			desq(x * dim, y *dim ,dim , 1);
 //----------------------------------	
-		int pos1 = (int)(Mouse.getX()/w);
-		int pos2 = (int)((soft.getHeight()-Mouse.getY())/w);
-		dfsq(w*pos1 ,w*pos2,w+1,1,c2);
-		//dbsos(m , pos1 , pos2, w+1 );
+		int pos1 = (int)(Mouse.getX()/dim);
+		int pos2 = (int)((soft.getHeight()-Mouse.getY())/dim);
+		
+		if(pos1 <= width && pos2 <= height)
+			dfsq(dim*pos1 ,dim*pos2,dim+1,1,c2);
+		addtts(mousePos , pos1 , pos2);
+		dms(mousePos , dim);
+		
+		while(Keyboard.next())
+		{	
+			if(Keyboard.getEventKey() == Keyboard.KEY_C)
+				mousePos = rm(mousePos);
+			
+		}
+				
 	}
 
     public static void main(String[] argv) {
     	Main main = new Main();
     	main.initFuncs();
+		int lastwheel; 
     	while(soft.isRunning())
     	{
     		soft.begin();
-    			main.runnable(30);
+    		dim = dim + Mouse.getDWheel()/100;
+    		if(dim <= 1)
+    			dim = 1;
+    		main.runnable(dim);
     		soft.end();
     	}
     	soft.finish();
